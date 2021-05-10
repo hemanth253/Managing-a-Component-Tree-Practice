bb4ee5eff8c8f433db49763a4d5653ba36c35cb3
hemanth253 <83533561+hemanth253@users.noreply.github.com>
2021-05-11 01:00:09 +0530

    v1

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-function InputArea() {
+function InputArea(props) {
   return (
     <div className="form">
-      <input onChange={handleChange} type="text" value={inputText} />
-      <button onClick={addItem}>
+      <input
+        onChange={() => {
+          return props.handleChangeParam();
+        }}
+        type="text"
+        value={props.inputTextParam}
+      />
+      <button
+        onClick={() => {
+          return props.addItemParam();
+        }}
+      >
         <span>Add</span>
       </button>
     </div>
